6bacdd1ab3bc2d3411fd217725dd8b867426d32c
Alex-Movchan <alexandr.movch@gmail.com>
2017-09-10 11:07:03 +0300

    test 2 clients

--- server.c
+++ server.c
@@ -5,14 +5,43 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int     sendtoall(char *msg, int *socks, int len_sock, int len_msg)
+{
+        int     n;
+        int     i;
+
+        i = -1;
+        while (++i < len_sock)
+        {
+                if (socks[i] > 0)
+                {
+                        if ((n = send(socks[i], msg, len_msg, 0)) == -1)
+                                return (-1);
+                }
+        }
+        return (n);
+}
+
+void	closeall(int *socks, int len)
+{
+	int	i;
+
+	i = -1;
+	while (++i < len)
+		close (socks[i]);
+}
+
 int	main(int ac, char **av)
 {
-	int			sock;
+	int			socks[10];
 	int			listener;
 	struct sockaddr_in	addr;
 	char			buf[1024];
 	int			bytes_read;
+	int			i;
+	int			sock;
 
+	i = 0;
 	listener = socket(AF_INET, SOCK_STREAM, 0);
 	if (listener < 0)
 	{
@@ -20,7 +49,7 @@ int	main(int ac, char **av)
 		exit(1);
 	}
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(atoi(av[1]));// или любой другой порт соответствующий клиенту
+	addr.sin_port = htons(atoi(av[1]));
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	if (bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
 	{
@@ -31,6 +60,8 @@ int	main(int ac, char **av)
  	while (1)
 	{
 		sock = accept(listener, NULL, NULL);
+		socks[i] = sock;
+		i++;
 		if (sock < 0)
 		{
 			perror("accept");
@@ -45,15 +76,16 @@ int	main(int ac, char **av)
 				close (listener);
 				while (1)
 				{
-					bytes_read = recv(sock, buf, 1024, 0);
+					bytes_read = recv(sock,  buf, 1024, 0);
 					if (bytes_read <= 0)
 						break;
-					send(sock, buf, bytes_read, 0);
+					sendtoall( buf, socks, i, bytes_read);
+					buf[0] = '\0';
 				}
 				close(sock);
 				exit (0);
 			defult :
-				close (sock);
+				closeall(socks, i);
 		}
 	}
 	return (0);
